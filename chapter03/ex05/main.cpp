d79183bc55fe0f9f7d5e7d5ab4bcef6a7b84be8f
Kotaro666-dev <kotaro0726@gmail.com>
2021-03-03 16:00:37 +0900

    Test: This is test in message
    This is test commit in body.

--- chapter03/ex05/main.cpp
+++ chapter03/ex05/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 typedef long long ll;
 const int INF = 1000000000;
-const ll LINF = 1000000000000000000;//1e18
+const ll LINF = 1000000000000000000; //1e18
 const double EPS = 1e-10;
 
 int main(void)
