b680dd7e6cdc1d90aee3c3c79eebbccf83faf0f7
Kotaro666-dev <kotaro0726@gmail.com>
2021-03-04 09:39:07 +0900

    Submit: Submit ex05
    Finish ex05 and submit it.

--- chapter03/ex05/main.cpp
+++ chapter03/ex05/main.cpp
@@ -6,8 +6,37 @@ const int INF = 1000000000;
 const ll LINF = 1000000000000000000; //1e18
 const double EPS = 1e-10;
 
+static int count_action()
+{
+	int N;
+	cin >> N;
+
+	vector<int> array(N);
+	for (int i = 0; i < N; i++)
+	{
+		cin >> array[i];
+		if (array[i] % 2 != 0)
+		{
+			return (0);
+		}
+	}
+	int counter = 1;
+	while (true)
+	{
+		for (int i = 0; i < N; i++)
+		{
+			array[i] /= 2;
+			if (array[i] % 2 != 0)
+			{
+				return (counter);
+			}
+		}
+		counter++;
+	}
+}
+
 int main(void)
 {
-
+	printf("answer = %d\n", count_action());
 	return (0);
 }
